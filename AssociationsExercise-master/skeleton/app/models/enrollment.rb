1a6a49a30e11a358adaf294c98f0204eae039cc3
spiffysparrow <tracykmullen@gmail.com>
2015-11-11 14:11:30 -0800

    Finished associations exercises

--- AssociationsExercise-master/skeleton/app/models/enrollment.rb
+++ AssociationsExercise-master/skeleton/app/models/enrollment.rb
@@ -1,2 +1,15 @@
 class Enrollment < ActiveRecord::Base
+  belongs_to(
+    :course,
+    foreign_key: :course_id,
+    primary_key: :id,
+    class_name: "Course"
+    )
+
+  belongs_to(
+    :student,
+    foreign_key: :student_id,
+    primary_key: :id,
+    class_name: "User"
+  )
 end
--- AssociationsExercise-master/skeleton/app/models/user.rb
+++ AssociationsExercise-master/skeleton/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    foreign_key: :student_id,
+    primary_key: :id,
+    class_name: "Enrollment"
+  )
+
+  has_many(
+    :enrolled_courses,
+    through: :enrollments,
+    source: :course
+  )
 end
